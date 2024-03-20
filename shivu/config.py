6380b67c39a3f0dfd602358f34cf7a87a9fd2b42
darkmadara09 <136217534+darkmadara09@users.noreply.github.com>
2024-03-20 14:12:12 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,16 +2,16 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6758236533"
-    sudo_users = "6758236533", "6758236533"
-    GROUP_ID = -1002074852216
-    TOKEN = "6684851281:AAGgGgYXwzoXKAQ21S2Z9oUCl3Gx8byqlfU"
+    OWNER_ID = "6529892817"
+    sudo_users = "6529892817", "6693744800"
+    GROUP_ID = -1002139608040
+    TOKEN = "6859073442:AAFFCb2qA648IETNkpgASW8h-gKSQZx-nZg"
     mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
-    PHOTO_URL = ["https://graph.org/file/722b32b3a4bb4a882759b.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "animefed2"
-    UPDATE_CHAT = "Ix_Updates"
-    BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = "-1002141220919"
+    PHOTO_URL = ["https://telegra.ph/file/a8f201f8d09943dc57a86.jpg", "https://telegra.ph/file/a8f201f8d09943dc57a86.jpg"]
+    SUPPORT_CHAT = "Muichiro_support"
+    UPDATE_CHAT = "Muichiro_Updates"
+    BOT_USERNAME = "Grab_carsbot"
+    CHARA_CHANNEL_ID = "-1002082423339"
     api_id = 20967639
     api_hash = "f0cc55917e860f93d2c9aaa4a7caa69c"
 
