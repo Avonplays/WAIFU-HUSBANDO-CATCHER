3f3673e58b4fd3dce29d14755c371d69dacebae7
darkmadara09 <136217534+darkmadara09@users.noreply.github.com>
2024-03-20 15:48:55 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -6,7 +6,7 @@ class Config(object):
     sudo_users = "6529892817", "6693744800"
     GROUP_ID = -1002139608040
     TOKEN = "6859073442:AAFFCb2qA648IETNkpgASW8h-gKSQZx-nZg"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
+    mongo_url = "mongodb+srv://Teamix1:teamix1@cluster0.y7h1qxl.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/a8f201f8d09943dc57a86.jpg", "https://telegra.ph/file/a8f201f8d09943dc57a86.jpg"]
     SUPPORT_CHAT = "Muichiro_support"
     UPDATE_CHAT = "Muichiro_Updates"
